826a3983bb09553eba621c281ca1744ec8a1897c
Sebastian Weber <s-weber85@gmx.de>
2024-06-19 09:56:00 +0200

    Registration.js created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import LandingPage from './pages/LandingPage';
+import Registration from './pages/Registration';
 
 function App() {
   return (
     <>
       <LandingPage />
+      <Registration/>
     </>
   );
 }
---
+++ frontend/src/pages/Registration.jsx
@@ -0,0 +1,14 @@
+
+
+function Registration() {
+  return (
+    <>
+        <div className="relative grid h-screen min-h-screen place-content-center place-items-center overflow-hidden bg-gray-950 px-4 py-24 pt-40 text-gray-200">
+            {/* <div className="absolute left-0 top-0 grid h-[450px] w-[350px] select-none place-content-center space-y-6 rounded-2xl border-2 border-slate-700 bg-slate-800/20 p-6 shadow-xl backdrop-blur-md ${
+            draggable ? 'cursor-grab active:cursor-grabbing"></div> */}
+        </div>
+    </>
+  );
+}
+
+export default Registration;
