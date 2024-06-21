3e277318edf9ca7542071f3d528cdf87d877e12f
AlexHess <alexhess104@gmail.com>
2024-06-21 16:46:41 +0200

    Added Profile Page Prototyp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import Onboarding from './pages/Onboarding';
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </>
   );
