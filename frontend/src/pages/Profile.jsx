7e2293d4b844388e36575bceb5543c41202a6d1e
Renke Brixel <93048457+ReynkeDeVos@users.noreply.github.com>
2024-06-24 09:31:56 +0200

    chore: Clean up code from errors

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -4,7 +4,7 @@ const Profile = () => {
   return (
     <div className="max-w-md rounded bg-black p-4 pt-8 shadow-md">
       {/* a Tag muss noch durch Link gewechselt werden */}
-      <a href="/" class="mr-2 font-medium text-teal-800">
+      <a href="/" className="mr-2 font-medium text-teal-800">
         Back
       </a>
       <div className="mb-6 flex justify-between">
@@ -51,12 +51,12 @@ const Profile = () => {
               viewBox="0 -2.5 256 256"
               version="1.1"
               xmlns="http://www.w3.org/2000/svg"
-              xmlns:xlink="http://www.w3.org/1999/xlink"
+              xlinkHref="http://www.w3.org/1999/xlink"
               preserveAspectRatio="xMidYMid"
               fill="#000000"
               transform="rotate(90)">
-              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-              <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+              <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
               <g id="SVGRepo_iconCarrier">
                 {' '}
                 <g fill="#702848">
@@ -97,13 +97,13 @@ const Profile = () => {
           <div className="flex flex-row items-center">
             {/* SVG Training Weeks */}
             <svg width="64px" height="64px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-              <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+              <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
               <g id="SVGRepo_iconCarrier">
                 {' '}
                 <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
                   d="M10.0284 1.11813C9.69728 1.2952 9.53443 1.61638 9.49957 1.97965C9.48456 2.15538 9.46201 2.32986 9.43136 2.50363C9.3663 2.87248 9.24303 3.3937 9.01205 3.98313C8.5513 5.15891 7.67023 6.58926 5.96985 7.65195C3.57358 9.14956 2.68473 12.5146 3.06456 15.527C3.45234 18.6026 5.20871 21.7903 8.68375 22.9486C9.03 23.0641 9.41163 22.9817 9.67942 22.7337C10.0071 22.4303 10.0238 22.0282 9.94052 21.6223C9.87941 21.3244 9.74999 20.5785 9.74999 19.6875C9.74999 19.3992 9.76332 19.1034 9.79413 18.8068C10.3282 20.031 11.0522 20.9238 11.7758 21.5623C12.8522 22.5121 13.8694 22.8574 14.1722 22.9466C14.402 23.0143 14.6462 23.0185 14.8712 22.9284C17.5283 21.8656 19.2011 20.4232 20.1356 18.7742C21.068 17.1288 21.1993 15.3939 20.9907 13.8648C20.7833 12.3436 20.2354 10.9849 19.7537 10.0215C19.3894 9.29292 19.0534 8.77091 18.8992 8.54242C18.7101 8.26241 18.4637 8.04626 18.1128 8.00636C17.8332 7.97456 17.5531 8.06207 17.3413 8.24739L15.7763 9.61686C15.9107 7.44482 15.1466 5.61996 14.1982 4.24472C13.5095 3.24609 12.7237 2.47913 12.1151 1.96354C11.8094 1.70448 11.5443 1.50549 11.3525 1.36923C11.2564 1.30103 11.1784 1.24831 11.1224 1.21142C10.7908 0.99291 10.3931 0.923125 10.0284 1.11813ZM7.76396 20.256C7.75511 20.0744 7.74999 19.8842 7.74999 19.6875C7.75 18.6347 7.89677 17.3059 8.47802 16.0708C8.67271 15.6572 8.91614 15.254 9.21914 14.8753C9.47408 14.5566 9.89709 14.4248 10.2879 14.5423C10.6787 14.6598 10.959 15.003 10.9959 15.4094C11.2221 17.8977 12.2225 19.2892 13.099 20.0626C13.5469 20.4579 13.979 20.7056 14.292 20.8525C15.5 20.9999 17.8849 18.6892 18.3955 17.7882C19.0569 16.6211 19.1756 15.356 19.0091 14.1351C18.8146 12.7092 18.2304 11.3897 17.7656 10.5337L14.6585 13.2525C14.3033 13.5634 13.779 13.5835 13.401 13.3008C13.023 13.018 12.8942 12.5095 13.092 12.0809C14.4081 9.22933 13.655 6.97987 12.5518 5.38019C12.1138 4.74521 11.6209 4.21649 11.18 3.80695C11.0999 4.088 10.9997 4.39262 10.8742 4.71284C10.696 5.16755 10.4662 5.65531 10.1704 6.15187C9.50801 7.26379 8.51483 8.41987 7.02982 9.34797C5.57752 10.2556 4.71646 12.6406 5.04885 15.2768C5.29944 17.2643 6.20241 19.1244 7.76396 20.256Z"
                   fill="#702848"></path>{' '}
               </g>
@@ -123,15 +123,15 @@ const Profile = () => {
               viewBox="0 0 24 24"
               version="1.1"
               xmlns="http://www.w3.org/2000/svg"
-              xmlns:xlink="http://www.w3.org/1999/xlink"
+              xlinkHref="http://www.w3.org/1999/xlink"
               fill="#000000">
-              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-              <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+              <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
               <g id="SVGRepo_iconCarrier">
                 <title>ic_fluent_flash_on_24_regular</title> <desc>Created with Sketch.</desc>{' '}
-                <g id="🔍-Product-Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <g id="🔍-Product-Icons" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
                   {' '}
-                  <g id="ic_fluent_flash_on_24_regular" fill="#702848" fill-rule="nonzero">
+                  <g id="ic_fluent_flash_on_24_regular" fill="#702848" fillRule="nonzero">
                     {' '}
                     <path
                       d="M8.29400141,14 L6.52697684,21.0680983 C6.34050874,21.8139707 7.26339156,22.3240198 7.79572333,21.7692956 L19.7911396,9.2692956 C20.2486433,8.79254712 19.9107562,8 19.25,8 L14.7905694,8 L16.4615125,2.98717082 C16.6233953,2.50152249 16.2619183,2 15.75,2 L8.75,2 C8.41513973,2 8.12085023,2.22198299 8.02885704,2.54395915 L5.02885704,13.0439592 C4.89196785,13.5230713 5.25171584,14 5.75,14 L8.29400141,14 Z M13.0384875,8.51282918 C12.8766047,8.99847751 13.2380817,9.5 13.75,9.5 L17.4908021,9.5 L8.6662195,18.6957861 L9.98219059,13.4319017 C10.1005306,12.9585417 9.74251208,12.5 9.25458372,12.5 L6.74429749,12.5 L9.31572606,3.5 L14.7094306,3.5 L13.0384875,8.51282918 Z"
@@ -154,22 +154,22 @@ const Profile = () => {
               version="1.0"
               id="Layer_1"
               xmlns="http://www.w3.org/2000/svg"
-              xmlns:xlink="http://www.w3.org/1999/xlink"
+              xlinkHref="http://www.w3.org/1999/xlink"
               width="64px"
               height="64px"
               viewBox="0 0 64.00 64.00"
-              enable-background="new 0 0 64 64"
-              xml:space="preserve"
+              enableBackground="new 0 0 64 64"
+              xmlSpace="preserve"
               fill="#702848"
               stroke="#702848"
-              stroke-width="1.408">
-              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              strokeWidth="1.408">
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
               <g
                 id="SVGRepo_tracerCarrier"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 stroke="#CCCCCC"
-                stroke-width="0.384"></g>
+                strokeWidth="0.384"></g>
               <g id="SVGRepo_iconCarrier">
                 {' '}
                 <g>
