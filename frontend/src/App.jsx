970caa6dbd2bb0ea6a08b64959cc87ea8507f28a
Renke Brixel <93048457+ReynkeDeVos@users.noreply.github.com>
2024-06-24 20:23:52 +0200

    added zxcvbn-ts for password strength calculation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,14 +12,15 @@ import SetYourGrind from './pages/SetYourGrind';
 import SplashScreen from './pages/SplashScreen';
 import UIElements from './pages/UIElements';
 import Workouts from './pages/Workouts';
-import Login from './pages/LogIn';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+
         <Route path="/onboarding" element={<Onboarding />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/profilerework" element={<Profilerework />} />
