6248194eb87106dd4ab721f40b63c36c7585969e
Renke Brixel <93048457+ReynkeDeVos@users.noreply.github.com>
2024-06-24 11:09:27 +0200

    chore: Update fonts in index.css and tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,20 @@ export default {
     extend: {
       fontFamily: {
         cthulhumbus: ['JmhCthulhumbus', 'sans-serif'],
+        HeadlOneHPLHS: ['HeadlOneHPLHS', 'sans-serif'],
+        OldStyHPLHS: ['OldStyHPLHS', 'sans-serif'],
+        OldstItaHPLHS: ['OldstItaHPLHS', 'sans-serif'],
+        OldstSmaCapHPLHS: ['OldstSmaCapHPLHS', 'sans-serif'],
+      },
+      colors: {
+        background: 'rgba(var(--background))',
+        border: 'rgba(var(--border))',
+        card: 'rgba(var(--card))',
+        'copy-primary': 'rgba(var(--copy-primary))',
+        'copy-secondary': 'rgba(var(--copy-secondary))',
+        cta: 'rgba(var(--cta))',
+        'cta-active': 'rgba(var(--cta-active))',
+        'cta-text': 'rgba(var(--cta-text))',
       },
     },
   },
