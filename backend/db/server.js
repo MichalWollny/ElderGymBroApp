8185e1645f8c517f1594c099d2a743b63426585b
Renke Brixel <93048457+ReynkeDeVos@users.noreply.github.com>
2024-06-25 15:00:54 +0200

    updated a tiny little bit of styling of SignUp Button & enforced a dark theme. Works medium well.

--- backend/db/server.js
+++ backend/db/server.js
@@ -1,13 +1,13 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const dbConnection = async () => {
   //   console.log(process.env.mongoDBURI);
   try {
     const connection = await mongoose.connect(process.env.mongoDBURI);
-    console.log("Connected to MongoDB");
+    console.log('Connected to MongoDB');
     console.log(connection.connection.db.databaseName);
   } catch (error) {
-    console.log("Connection errror:", error.stack);
+    console.log('Connection errror:', error.stack);
   }
 };
 dbConnection();
