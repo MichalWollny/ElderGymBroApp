aabc3b60c3e0d76557b5f1298df005959f3af686
Cain497 <cain497@gmail.com>
2024-06-25 14:52:04 +0200

    routes fur Profile updates ready and working

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose';
 import { getTitle } from '../utils/karmaUtils.js';
+import { getDefaultAvatar } from '../utils/profileUtils.js';
 
 const userSchema = new mongoose.Schema(
   {
     fullName: {
       type: String,
-      required: [true, 'Name is required'],
+      // required: [true, 'Name is required'],
+      default: 'unknown',
     },
     username: {
       type: String,
@@ -22,30 +24,24 @@ const userSchema = new mongoose.Schema(
       required: [true, 'Password is required'],
       select: false,
     },
-    age: { type: Number },
-    weight: { type: Number },
-    gender: { type: String, enum: ['male', 'female', 'elder thing', 'blob', 'other'] },
+    age: { type: Number, default: null },
+    weight: { type: Number, default: null },
+    gender: { type: String, default: '', enum: ['', 'male', 'female', 'elder thing', 'blob', 'other'] },
     fitnesLevel: {
       type: String,
-      required: [true, 'Please state your current fitness level'],
+      // required: [true, 'Please state your current fitness level'],
+      default: 'beginner',
       enum: ['beginner', 'intermediate', 'advanced'],
     },
-    workoutAim: { type: String, enum: ['Muscle Worship', 'Fat Fight', 'Stamina Destruction', 'Cardio Crusade'] },
+    workoutAim: {
+      type: String,
+      default: '',
+      enum: ['', 'Muscle Worship', 'Fat Fight', 'Stamina Destruction', 'Cardio Crusade'],
+    },
     avatar: {
       type: String,
       default: function () {
-        switch (this.gender) {
-          case 'male':
-            return 'https://i.pinimg.com/736x/3e/42/b1/3e42b1c802226a211c690c9b02d06ffd.jpg';
-          case 'female':
-            return 'https://i.seadn.io/gae/k01LjPxdfW0LhgSjNksj3KhPIbskLJb-B27latpNhb9fHEJqcoOlVX9RcRInKdQTWekaTF70-tGzxv_F_wACE-77Dlm1qSH4kayE-A?auto=format&dpr=1&w=1000';
-          case 'elder thing':
-            return 'https://i.pinimg.com/474x/6c/92/36/6c923664f3c5fa0715fa82627a2fa04e.jpg';
-          case 'blob':
-            return 'https://e7.pngegg.com/pngimages/869/87/png-clipart-shoggoth-%E3%81%84%E3%82%89%E3%81%99%E3%81%A8%E3%82%84-illustrator-cthulhu-mythos-cthulhu-illustrator-fictional-character.png';
-          case 'other':
-            return 'https://t3.ftcdn.net/jpg/05/59/68/54/360_F_559685420_ZyC3yn4khaJ6gptajKVUPAs1Bl8kUvEL.jpg';
-        }
+        return getDefaultAvatar(this.gender);
       },
     },
     awards: {
