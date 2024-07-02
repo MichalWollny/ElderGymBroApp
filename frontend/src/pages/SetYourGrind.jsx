0037168a06e820d872092f8c9c9fd2bba0635ca0
AlexHess <alexhess104@gmail.com>
2024-07-02 16:37:22 +0200

    forgot save

--- frontend/src/pages/SetYourGrind.jsx
+++ frontend/src/pages/SetYourGrind.jsx
@@ -81,7 +81,7 @@ function SetYourGrind() {
           <CardMedia
             sx={{
               width: '100vw', // take up full width
-              height: '28vh', // take up half height
+              height: '28vh',
               objectFit: 'fit', // make the image cover the entire container
             }}
             image={setgrind}
