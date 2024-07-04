b8e6c0f2e07bced38fe3a032eda7d923a93e8ac2
Renke Brixel <93048457+ReynkeDeVos@users.noreply.github.com>
2024-07-04 10:06:29 +0200

    Update page title from "Trophys" to "Trophies" and fix typo in progress percentage text

--- frontend/src/pages/LoginForm.jsx
+++ frontend/src/pages/LoginForm.jsx
@@ -1,17 +1,38 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { useAuth } from '../context/AuthProvider';
 import { Container, Typography, TextField, Button, Grid } from '@mui/material';
 // import { toast } from 'react-toastify';
 
+import { Stars } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+
+import { useMotionTemplate, useMotionValue, motion, animate } from 'framer-motion';
+
+// colors for the background gradient
+const COLORS_TOP = useMemo(() => ['#13FFAA', '#1E67C6', '#CE84CF', '#DD335C'], []);
+
 function LoginForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const { setIsLoggedIn, checkUser } = useAuth();
+  // for the background gradient
+  const color = useMotionValue(COLORS_TOP[0]);
 
   const navigate = useNavigate();
+
+  useEffect(() => {
+    animate(color, COLORS_TOP, {
+      ease: 'easeInOut',
+      duration: 10,
+      repeat: Infinity,
+      repeatType: 'mirror',
+    });
+  }, [color, COLORS_TOP]);
+
+  const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 0%, #020617 50%, ${color})`;
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -37,104 +58,78 @@ function LoginForm() {
   };
 
   return (
-    <Container
-      maxWidth="sm"
-      sx={{
-        height: '100svh', // Make the Container fill the entire viewport height
-        display: 'flex',
-        flexDirection: 'column', // Stack children vertically
-        justifyContent: 'flex-start', // Align children to the start of the container
-        '& .MuiTextField-root': {
-          mb: 2, // Default margin bottom
-          '@media (max-width:600px)': {
-            mb: 1, // Reduced margin bottom for screens smaller than 600px
-          },
-        },
-        '& .MuiButton-root': {
-          mt: 3,
-        },
+    <motion.section
+      style={{
+        backgroundImage,
       }}>
-      <Typography variant="h4" gutterBottom>
-        Login
-      </Typography>
-      <form onSubmit={handleLogin}>
-        <Grid container spacing={0}>
-          <Grid item xs={12}>
-            <TextField
-              type="text"
-              label="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              className="w-full rounded border p-2"
-            />
-          </Grid>
+      <Container
+        maxWidth="sm"
+        sx={{
+          height: '100svh', // Make the Container fill the entire viewport height
+          display: 'flex',
+          flexDirection: 'column', // Stack children vertically
+          justifyContent: 'flex-start', // Align children to the start of the container
+          '& .MuiTextField-root': {
+            mb: 2, // Default margin bottom
+            '@media (max-width:600px)': {
+              mb: 1, // Reduced margin bottom for screens smaller than 600px
+            },
+          },
+          '& .MuiButton-root': {
+            mt: 3,
+          },
+        }}>
+        <Typography variant="h4" gutterBottom>
+          Login
+        </Typography>
+        <form onSubmit={handleLogin}>
+          <Grid container spacing={0}>
+            <Grid item xs={12}>
+              <TextField
+                type="text"
+                label="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                className="w-full rounded border p-2"
+              />
+            </Grid>
 
-          <Grid item xs={12}>
-            <TextField
-              type="password"
-              label="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              className="w-full rounded border p-2"
-            />
+            <Grid item xs={12}>
+              <TextField
+                type="password"
+                label="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                className="w-full rounded border p-2"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                type="submit"
+                variant="contained"
+                fullWidth
+                sx={{ mt: 3, mb: 2, bgcolor: 'teal', color: 'white' }}>
+                Login
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <Button type="submit" variant="contained" fullWidth sx={{ mt: 3, mb: 2, bgcolor: 'teal', color: 'white' }}>
-              Login
-            </Button>
-          </Grid>
-        </Grid>
-      </form>
-      <p className="mt-2 text-center text-xs text-slate-400">
-        Not registered yet?{' '}
-        <Link to="/register" className="text-teal-600 underline">
-          Register here
-        </Link>
-      </p>
-    </Container>
+        </form>
+        <p className="mt-2 text-center text-xs text-slate-400">
+          Not registered yet?{' '}
+          <Link to="/register" className="text-teal-600 underline">
+            Register here
+          </Link>
+        </p>
+        <div className="absolute inset-0 z-[-1]">
+          <Canvas>
+            <Stars radius={50} count={2500} factor={4} fade speed={2} />
+          </Canvas>
+        </div>
+      </Container>
+    </motion.section>
   );
 }
 
 export default LoginForm;
-
-{
-  /* <div className="container mx-auto mt-8 max-w-md rounded-xl shadow-xl shadow-gray-500">
-  <div className="p-4">
-    <h2 className="mb-4 text-2xl font-semibold">Login</h2>
-    {error && <p className="mb-4 text-red-500">{error}</p>}
-    <form onSubmit={handleLogin}>
-      <div className="mb-4">
-        <label className="mb-2 block">Email:</label>
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          className="w-full rounded border p-2"
-        />
-      </div>
-      <div className="mb-4">
-        <label className="mb-2 block">Password:</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          className="w-full rounded border p-2"
-        />
-      </div>
-      <Button type="submit" variant="contained" fullWidth sx={{ mt: 3, mb: 2, bgcolor: 'teal', color: 'white' }}>
-        Login
-      </Button>
-    </form>
-    <p className="mt-2 text-center text-xs text-slate-400">
-      Not registered yet?{' '}
-      <Link to="/register" className="text-teal-600 underline">
-        Register here
-      </Link>
-    </p>
-  </div>
-</div>; */
-}
--- frontend/src/pages/RegisterForm.jsx
+++ frontend/src/pages/RegisterForm.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useState, useEffect, useDeferredValue } from 'react';
+import { useState, useEffect, useDeferredValue, useMemo } from 'react';
 import {
   Container,
   Typography,
@@ -20,6 +20,11 @@ import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
+
+import { Stars } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+
+import { useMotionTemplate, useMotionValue, motion, animate } from 'framer-motion';
 
 // Configure zxcvbn-ts
 const matcherPwned = matcherPwnedFactory(fetch, zxcvbnOptions);
@@ -94,7 +99,9 @@ function RegisterForm() {
     event.preventDefault();
   };
 
-  // when the form is submitted, send a POST request to the server
+  // colors for the background gradient
+  const COLORS_TOP = useMemo(() => ['#13FFAA', '#1E67C6', '#CE84CF', '#DD335C'], []);
+
   const handleRegister = async (e) => {
     e.preventDefault(); // Prevent default form submission behavior
     try {
@@ -117,135 +124,163 @@ function RegisterForm() {
     }
   };
 
+  const color = useMotionValue(COLORS_TOP[0]);
+
+  useEffect(() => {
+    animate(color, COLORS_TOP, {
+      ease: 'easeInOut',
+      duration: 10,
+      repeat: Infinity,
+      repeatType: 'mirror',
+    });
+  }, [color, COLORS_TOP]);
+
+  const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 0%, #020617 50%, ${color})`;
+
   return (
-    <Container
-      maxWidth="sm"
-      sx={{
-        // bgcolor: 'background.default',
-        // color: 'text.primary',
-        height: '100svh', // Make the Container fill the entire viewport height
-        display: 'flex',
-        flexDirection: 'column', // Stack children vertically
-        justifyContent: 'flex-start', // Align children to the start of the container
-        '& .MuiTextField-root': {
-          mb: 2, // Default margin bottom
-          '@media (max-width:600px)': {
-            mb: 1, // Reduced margin bottom for screens smaller than 600px
-          },
-        },
-        '& .MuiButton-root': {
-          mt: 3,
-        },
+    <motion.section
+      style={{
+        backgroundImage,
       }}>
-      <Typography variant="h4" gutterBottom>
-        Register
-      </Typography>
-      <form onSubmit={handleRegister}>
-        <Grid container spacing={0}>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              required
-              label="Username"
-              autoComplete="username"
-              name="username"
-              value={formData.username}
-              onChange={handleChange}
-            />
+      <Container
+        maxWidth="sm"
+        sx={{
+          // bgcolor: 'background.default',
+          // color: 'text.primary',
+
+          height: '100svh', // Make the Container fill the entire viewport height
+          display: 'flex',
+          flexDirection: 'column', // Stack children vertically
+          justifyContent: 'flex-start', // Align children to the start of the container
+          '& .MuiTextField-root': {
+            mb: 2, // Default margin bottom
+            '@media (max-width:600px)': {
+              mb: 1, // Reduced margin bottom for screens smaller than 600px
+            },
+          },
+          '& .MuiButton-root': {
+            mt: 3,
+          },
+        }}>
+        <Typography variant="h4" gutterBottom>
+          Register
+        </Typography>
+        <form onSubmit={handleRegister}>
+          <Grid container spacing={0}>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                required
+                label="Username"
+                autoComplete="username"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                required
+                label="Email"
+                autoComplete="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                required
+                label="Password"
+                type={showPassword ? 'text' : 'password'}
+                name="password"
+                autoComplete="new-password"
+                variant="outlined"
+                value={formData.password}
+                onChange={handleChange}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}>
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              {result && (
+                <>
+                  <LinearProgress
+                    variant="determinate"
+                    value={result.score * 25}
+                    color={getPasswordStrengthColor(result.score)}
+                    sx={{ borderRadius: 4 }}
+                  />
+                  {result.feedback.warning && <Alert severity="warning">{result.feedback.warning}</Alert>}
+                  {result.feedback.suggestions.length > 0 &&
+                    result.feedback.suggestions.map((suggestion, index) => (
+                      <Alert key={index} severity="info">
+                        {suggestion}
+                      </Alert>
+                    ))}
+                </>
+              )}
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                required
+                label="Confirm Password"
+                autoComplete="new-password"
+                type={showPassword ? 'text' : 'password'}
+                name="confirmPassword"
+                sx={{ mt: 1.2 }}
+                variant="outlined"
+                value={formData.confirmPassword}
+                onChange={handleChange}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}>
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                type="submit"
+                variant="contained"
+                fullWidth
+                sx={{ mt: 3, mb: 2, bgcolor: 'teal', color: 'white' }}>
+                Register
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              required
-              label="Email"
-              autoComplete="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              required
-              label="Password"
-              type={showPassword ? 'text' : 'password'}
-              name="password"
-              autoComplete="new-password"
-              variant="outlined"
-              value={formData.password}
-              onChange={handleChange}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}>
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-            {result && (
-              <>
-                <LinearProgress
-                  variant="determinate"
-                  value={result.score * 25}
-                  color={getPasswordStrengthColor(result.score)}
-                  sx={{ borderRadius: 4 }}
-                />
-                {result.feedback.warning && <Alert severity="warning">{result.feedback.warning}</Alert>}
-                {result.feedback.suggestions.length > 0 &&
-                  result.feedback.suggestions.map((suggestion, index) => (
-                    <Alert key={index} severity="info">
-                      {suggestion}
-                    </Alert>
-                  ))}
-              </>
-            )}
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              required
-              label="Confirm Password"
-              autoComplete="new-password"
-              type={showPassword ? 'text' : 'password'}
-              name="confirmPassword"
-              sx={{ mt: 1.2 }}
-              variant="outlined"
-              value={formData.confirmPassword}
-              onChange={handleChange}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}>
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button type="submit" variant="contained" fullWidth sx={{ mt: 3, mb: 2, bgcolor: 'teal', color: 'white' }}>
-              Register
-            </Button>
-          </Grid>
-        </Grid>
-      </form>
-      <p className="mt-2 text-center text-xs text-slate-400">
-        Already have an account?{' '}
-        <Link to="/login" className="text-teal-600 underline">
-          Login
-        </Link>
-      </p>
-    </Container>
+        </form>
+        <p className="mt-2 text-center text-xs text-slate-400">
+          Already have an account?{' '}
+          <Link to="/login" className="text-teal-600 underline">
+            Login
+          </Link>
+        </p>
+        <div className="absolute inset-0 z-[-1]">
+          <Canvas>
+            <Stars radius={50} count={2500} factor={4} fade speed={2} />
+          </Canvas>
+        </div>
+      </Container>
+    </motion.section>
   );
 }
 
