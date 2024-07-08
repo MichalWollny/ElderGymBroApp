e3684adc5e5a09566d38917d18fe9dde7de0edab
Renke Brixel <93048457+ReynkeDeVos@users.noreply.github.com>
2024-07-08 15:38:53 +0200

    chore: Update token expiration to 24 hours

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -70,7 +70,7 @@ export const signIn = asyncHandler(async (req, res, next) => {
     expiresIn: '30m',
   });
   // res.json({ token });
-  res.cookie('token', token, { maxAge: 1800000, httpOnly: true, sameSite: 'none', secure: true }); // 30min
+  res.cookie('token', token, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'none', secure: true }); // 24hrs
   res.send({ status: 'success' });
 });
 
