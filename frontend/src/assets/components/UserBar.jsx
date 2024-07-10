050e1cf3d3cd028342744010bb3302a4501366a1
Sebastian Weber <s-weber85@gmx.de>
2024-07-10 11:13:43 +0200

    "Refactor UserBar component: simplify navigation layout, update logo image styling, and adjust user data display"

--- frontend/src/assets/components/UserBar.jsx
+++ frontend/src/assets/components/UserBar.jsx
@@ -6,29 +6,11 @@ const UserBar = () => {
   const { userData, isLoggedIn } = useAuth();
 
   return (
-    <nav className="fixed top-0 z-50 flex w-full items-center justify-between bg-gray-900 py-2 font-cthulhumbus shadow-md">
-      <div className="flex w-full items-center justify-between px-4">
-        <div className="flex items-center">
-          <img src={logoImage} alt="App Logo" className="h-16 w-auto sm:h-12 sm:w-12 md:h-16 md:w-16" />
-        </div>
-        <div className="flex flex-1 flex-col items-center justify-center">
-          {isLoggedIn && userData ? (
-            <>
-              <div className="mt-2 cursor-default bg-gradient-to-br from-yellow-950 to-yellow-500 bg-clip-text text-center font-cthulhumbus text-xl font-medium leading-tight text-transparent sm:text-3xl md:text-4xl">
-                {userData.username || 'No username'}
-              </div>
-              <div className="mt-0 cursor-default text-nowrap bg-gradient-to-br from-teal-500 to-green-800 bg-clip-text text-center font-cthulhumbus text-lg font-medium leading-tight text-transparent sm:text-2xl md:text-xl">
-                {userData.awards?.title || 'No title'}
-              </div>
-            </>
-          ) : (
-            <p className="text-center text-teal-700">Loading...</p>
-          )}
-        </div>
-        <div className="flex flex-col items-center justify-center">
     <nav className="fixed top-0 z-50 flex w-full items-center justify-between bg-gray-900 py-2 font-cthulhumbus shadow-md cursor-default select-none">
       <div className="grid w-full grid-cols-3">
-        <div></div>
+        <div className="flex items-center">
+          <img src={logoImage} alt="App Logo" className="w-10 ml-5 mx-auto sm:w-12 md:w-24 md:mx-auto" />
+        </div>
         {isLoggedIn && userData ? (
           <div className="flex flex-col items-center justify-center">
             <div className="mt-1 cursor-default bg-gradient-to-br  from-teal-500 to-green-800 bg-clip-text text-center font-cthulhumbus font-medium leading-tight text-transparent text-xl sm:text-2xl md:text-3xl">
@@ -42,7 +24,7 @@ const UserBar = () => {
           <p className="text-center text-teal-700">Loading...</p>
         )}
         <div className="ml-12 -mt-2 flex flex-col justify-center">
-          <div className="mx-auto mt-2 w-8 sm:w-12 md:w-16">
+          <div className="mx-auto mt-4 w-8 sm:w-12 md:w-16">
             {isLoggedIn && userData ? (
               <StyledCircularProgressbar
                 value={userData.awards?.progress}
