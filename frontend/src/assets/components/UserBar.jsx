433b8ee4ab45a4964a342e20997873d728d11082
Renke Brixel <93048457+ReynkeDeVos@users.noreply.github.com>
2024-07-10 10:36:46 +0200

    added logo to top bar

--- frontend/src/assets/components/UserBar.jsx
+++ frontend/src/assets/components/UserBar.jsx
@@ -1,26 +1,31 @@
 import { useAuth } from '../../context/AuthProvider';
 import StyledCircularProgressbar from './CircularProgress';
+import logoImage from '../../assets/icons/elderGymBroLogo.png';
 
 const UserBar = () => {
   const { userData, isLoggedIn } = useAuth();
 
   return (
     <nav className="fixed top-0 z-50 flex w-full items-center justify-between bg-gray-900 py-2 font-cthulhumbus shadow-md">
-      <div className="grid w-full grid-cols-3">
-        <div></div>
-        {isLoggedIn && userData ? (
-          <div className="flex flex-col items-center justify-center">
-            <div className="mt-2 cursor-default bg-gradient-to-br from-yellow-950 to-yellow-500 bg-clip-text text-center font-cthulhumbus text-xl font-medium leading-tight text-transparent sm:text-3xl md:text-4xl">
-              {userData.username || 'No username'}
-            </div>
-            <div className="mt-0 cursor-default text-nowrap bg-gradient-to-br from-teal-500 to-green-800 bg-clip-text text-center font-cthulhumbus text-lg font-medium leading-tight text-transparent sm:text-2xl md:text-xl">
-              {userData.awards?.title || 'No title'}
-            </div>
-          </div>
-        ) : (
-          <p className="text-center text-teal-700">Loading...</p>
-        )}
-        <div className="ml-6 flex flex-col justify-center">
+      <div className="flex w-full items-center justify-between px-4">
+        <div className="flex items-center">
+          <img src={logoImage} alt="App Logo" className="h-16 w-auto sm:h-12 sm:w-12 md:h-16 md:w-16" />
+        </div>
+        <div className="flex flex-1 flex-col items-center justify-center">
+          {isLoggedIn && userData ? (
+            <>
+              <div className="mt-2 cursor-default bg-gradient-to-br from-yellow-950 to-yellow-500 bg-clip-text text-center font-cthulhumbus text-xl font-medium leading-tight text-transparent sm:text-3xl md:text-4xl">
+                {userData.username || 'No username'}
+              </div>
+              <div className="mt-0 cursor-default text-nowrap bg-gradient-to-br from-teal-500 to-green-800 bg-clip-text text-center font-cthulhumbus text-lg font-medium leading-tight text-transparent sm:text-2xl md:text-xl">
+                {userData.awards?.title || 'No title'}
+              </div>
+            </>
+          ) : (
+            <p className="text-center text-teal-700">Loading...</p>
+          )}
+        </div>
+        <div className="flex flex-col items-center justify-center">
           <div className="mx-auto mt-2 w-8 sm:w-12 md:w-16">
             {isLoggedIn && userData ? (
               <StyledCircularProgressbar
