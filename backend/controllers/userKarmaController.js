aeb6bf70db4874e3465e14b2a5e5b449ca721dcd
Cain497 <cain497@gmail.com>
2024-07-19 16:09:11 +0200

    Update userKarmaController.js
    -moved some functions from controllers to utils, so they can be imported when needed

--- backend/controllers/userKarmaController.js
+++ backend/controllers/userKarmaController.js
@@ -1,36 +1,9 @@
 import asyncHandler from '../utils/asyncHandler.js';
 import ErrorResponse from '../utils/ErrorResponse.js';
 import User from '../models/userSchema.js';
-import { getTitle } from '../utils/karmaUtils.js';
+import { getTitle, karmaPointsPerLevel, calculateLevel, calculateProgressToNextLevel } from '../utils/karmaUtils.js';
 
-// Assuming karmaPointsPerLevel is available in the scope
-const karmaPointsPerLevel = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
-
-function calculateLevel(currentKarmaPoints) {
-  let level = 1;
-  for (let i = 0; i < karmaPointsPerLevel.length; i++) {
-    if (currentKarmaPoints >= karmaPointsPerLevel[i]) {
-      level = i + 2; // Levels start at 1, so +2 to account for the next level
-    } else {
-      break;
-    }
-  }
-  return level;
-}
-function calculateProgressToNextLevel(currentKarmaPoints) {
-  // Assuming karmaPointsPerLevel defines the points needed for each level
-  // Find the next level's index
-  const nextLevelIndex = karmaPointsPerLevel.findIndex((points) => currentKarmaPoints < points);
-  if (nextLevelIndex === -1) {
-    // If the user's karma points exceed all defined levels, consider them at max progress
-    return 100; // Assuming 100% is max progress
-  }
-  // Calculate progress towards the next level
-  const pointsForNextLevel = karmaPointsPerLevel[nextLevelIndex];
-  const pointsForCurrentLevel = nextLevelIndex === 0 ? 0 : karmaPointsPerLevel[nextLevelIndex - 1];
-  const progress = ((currentKarmaPoints - pointsForCurrentLevel) / (pointsForNextLevel - pointsForCurrentLevel)) * 100;
-  return progress;
-}
+// Update Karma Points and Level
 
 export const updateKarmaFunction = async (req, res, next) => {
   const userId = req.uid;
