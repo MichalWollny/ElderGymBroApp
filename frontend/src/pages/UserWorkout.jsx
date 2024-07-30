640e61667824b0a45e3e6381ca75fe66f4a5aebd
Renke Brixel <93048457+ReynkeDeVos@users.noreply.github.com>
2024-07-30 17:32:23 +0200

    Adjusted modals: No white background anymore!

--- frontend/src/pages/UserWorkout.jsx
+++ frontend/src/pages/UserWorkout.jsx
@@ -184,11 +184,11 @@ const UserWorkout = () => {
     slidesToShow: 3,
     slidesToScroll: 1,
     centerMode: true,
-    centerPadding: '20%', // Increase padding to show parts of adjacent images
-    arrows: false, // Hide the default arrows
-    accessibility: true, // Enable keyboard navigation
-    focusOnSelect: true, // Focus on select to enable keyboard interaction
-    afterChange: handleAfterChange, // Add afterChange handler
+    centerPadding: '20%',
+    arrows: false,
+    accessibility: true,
+    focusOnSelect: true,
+    afterChange: handleAfterChange,
     responsive: [
       {
         breakpoint: 1024,
@@ -198,7 +198,7 @@ const UserWorkout = () => {
           infinite: false,
           dots: true,
           centerMode: true,
-          centerPadding: '20%', // Increase padding to show parts of adjacent images
+          centerPadding: '20%',
         },
       },
       {
@@ -207,7 +207,7 @@ const UserWorkout = () => {
           slidesToShow: 1,
           slidesToScroll: 1,
           centerMode: true,
-          centerPadding: '20%', // Increase padding to show parts of adjacent images
+          centerPadding: '20%',
         },
       },
     ],
@@ -227,15 +227,11 @@ const UserWorkout = () => {
         </div>
       </div>
       <div className="mt-6 w-full max-w-screen-sm">
-        {' '}
-        {/* Adjust margin to make space for the progress bar */}
         <h2 className="px-4 py-2 text-center font-cthulhumbus text-xl text-white">Exercise List</h2>
         {isLoading ? (
           <div>Loading...</div>
         ) : (
           <div className="pb-6">
-            {' '}
-            {/* Add padding to the bottom of the slider */}
             <Slider {...settings} className="max-w-full" ref={sliderRef}>
               {activeWorkout.exercises.map((exercise, index) => (
                 <div
@@ -244,9 +240,7 @@ const UserWorkout = () => {
                   className={`carousel-item flex flex-col items-center px-2 ${index === selectedIndex ? 'selected' : ''}`}
                   onClick={() => handleExerciseClick(exercise, index)}>
                   <img
-
                     src={importImage(exercise.name)}
-
                     alt={exercise.name}
                     className={`h-auto w-full max-w-xs rounded-md ${completedExercises.includes(index) ? 'grayscale' : ''}`}
                   />
@@ -263,34 +257,34 @@ const UserWorkout = () => {
             exercise={activeExercise}
             activeWorkout={activeWorkout}
             onComplete={completeExerciseWithKarma}
-            onCompleteWithoutKarma={handleCompleteExercise} // New prop for skipping without karma
+            onCompleteWithoutKarma={handleCompleteExercise}
             isCompleted={completedExercises.includes(selectedIndex)}
             setCompletedExercises={setCompletedExercises}
             completedExercises={completedExercises}
             selectedIndex={selectedIndex}
-            sliderRef={sliderRef} // Pass sliderRef
-            handleExerciseClick={handleExerciseClick} // Pass handleExerciseClick
-            setShowModal={setShowModal} // Pass setShowModal
+            sliderRef={sliderRef}
+            handleExerciseClick={handleExerciseClick}
+            setShowModal={setShowModal}
           />
         ) : (
           <p className="text-center text-white">Choose an Exercise</p>
         )}
       </div>
       {showModal && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-80">
           <Confetti
             width={window.innerWidth}
             height={window.innerHeight}
             numberOfPieces={1000}
             recycle={false}
-            className="absolute inset-0" // Confetti within the modal
+            className="absolute inset-0"
           />
-          <div className="relative z-10 w-full max-w-sm rounded-lg bg-white p-6 shadow-lg">
-            <h2 className="text-center text-2xl font-bold text-green-500">Well done!</h2>
-            <p className="mt-4 text-center text-green-500">You have completed all exercises.</p>
-            <p className="pb-4 text-center text-green-500">Total Karma Points Collected: {karmaPoints}</p>
+          <div className="relative z-10 w-full max-w-sm rounded-lg bg-gray-900 p-6 text-white shadow-lg">
+            <h2 className="text-center text-2xl font-bold text-teal-400">Well done!</h2>
+            <p className="mt-4 text-center text-teal-400">You have completed all exercises.</p>
+            <p className="pb-4 text-center text-teal-400">Total Karma Points Collected: {karmaPoints}</p>
             <img src={doneImage} alt="Well done" className="mx-auto mb-4 h-32 w-32" />
-            <button className="mt-6 w-full rounded-md bg-green-500 py-2 text-white" onClick={() => navigate('/home')}>
+            <button className="mt-6 w-full rounded-md bg-teal-700 py-2 text-white" onClick={() => navigate('/home')}>
               Completed
             </button>
           </div>
